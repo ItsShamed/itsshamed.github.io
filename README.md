c3dc8b13a395d31de0d394c322f6efe663b242fa
Ryuki <thedjplays.gaming@gmail.com>
2022-05-27 21:47:10 +0200

    add soundcloud

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # TsuRuki static page
 
-relly advence site wit complextro Java framworkege
+relly advence site wit complextro Java framworkege  
+soundcloud not having a branding page and not using vector logo is very
+cringege
--- js/index.js
+++ js/index.js
@@ -1,21 +1,25 @@
 let placeholder = document.getElementById("tooltip-placeholder");
 
 document.getElementById("github").addEventListener("mouseover",  ev => {
-    placeholder.textContent = "GitHub (ItsShamed)"
+    placeholder.textContent = "GitHub (ItsShamed)";
 })
 
 document.getElementById("yt").addEventListener("mouseover", ev => {
-    placeholder.textContent = "YouTube (Ruki)"
+    placeholder.textContent = "YouTube (Ruki)";
 })
 
 document.getElementById("twitch").addEventListener("mouseover",  ev => {
-    placeholder.textContent = "Twitch (Tsu_Ruki)"
+    placeholder.textContent = "Twitch (Tsu_Ruki)";
 })
 
 document.getElementById("twitter").addEventListener("mouseover", ev => {
-    placeholder.textContent = "Twitter (@ItsShamed)"
+    placeholder.textContent = "Twitter (@ItsShamed)";
 })
 
 document.getElementById("discord").addEventListener("mouseover", ev => {
-    placeholder.textContent = "Discord (Ruki#8873)"
+    placeholder.textContent = "Discord (Ruki#8873)";
 })
+
+document.getElementById("soundcloud").addEventListener("mouseover", ev => {
+    placeholder.textContent = "SoundCloud (Ruki / itsshamed)";
+}) 
